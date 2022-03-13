e04f8d43966a56d9c8530670a84b940f9c51ecad
Omar2535 <omar2535@alumni.ubc.ca>
2022-03-13 16:31:51 -0700

    add sensory detector stubs

---
+++ sensory-detector/modules/SensoryFile.py
@@ -0,0 +1,16 @@
+# Sensory file class
+
+class SensoryFile:
+    
+    def __init__(self, file_path):
+        # TODO: Create the file here
+        pass
+        
+        
+    def was_changed(self):
+        # TODO: Check if the file was changed / encrypted
+        pass
+        
+    def create_file(self, file_path):
+        # TODO: Create the file with static file contents
+        pass
---
+++ sensory-detector/readme.md
@@ -0,0 +1,5 @@
+# Sensory detector
+
+This detector module creates small "sensory" files on the file-system
+to periodically check if the file was encrypted. If the file was encrypted,
+then the program can conclude that the file was encrypted.
