55cc4f55f209492645ca2abde1f206e25b7e1d74
Omar2535 <omar2535@alumni.ubc.ca>
2022-03-20 19:49:23 -0700

    add docstrings

--- sensory-detector/main.py
+++ sensory-detector/main.py
@@ -1,9 +1,16 @@
 from modules.CustomLogger import CustomLogger
+from modules.SensoryFile import SensoryFile
 
 
 def main() -> None:
     logger = CustomLogger(__name__).get_logger()
     logger.info("Starting up Rescate-tance sensory detector")
+
+    sf = SensoryFile("./lol.txt")
+    sf.create()
+    breakpoint()
+    sf.check()
+    sf.delete()
 
     logger.info("Shutting down Rescate-tance sensory detector")
 
--- sensory-detector/modules/SensoryFile.py
+++ sensory-detector/modules/SensoryFile.py
@@ -1,7 +1,7 @@
 # Sensory file class
 
 from constants import SENSORY_FILE_CONTENTS
-from CustomLogger import CustomLogger
+from .CustomLogger import CustomLogger
 import os
 
 
@@ -10,20 +10,35 @@ class SensoryFile:
         self.logger = CustomLogger(__name__).get_logger()
         self.file_path = file_path
 
-    def check(self):
-        # TODO: Check if the file was changed / encrypted
-        pass
+    def check(self) -> bool:
+        """Check if the contents of sensor file is same. Throws exception if file was changed
+
+        Returns:
+            bool: _description_
+        """
+        f = open(f"{self.file_path}", "r")
+        content = f.read()
+        f.close()
+
+        if content == SENSORY_FILE_CONTENTS:
+            return
+        else:
+            raise Exception("File was changed!")
 
     def create(self):
+        """Create sensor file"""
         try:
             f = open(f"{self.file_path}", "x")
             f.write(SENSORY_FILE_CONTENTS)
             f.close()
+            self.logger.info(f"Sensory file {self.file_path} created")
         except FileExistsError:
             self.logger.warning(f"Creating file '{self.file_path}' already exists")
 
     def delete(self):
+        """Delete sensor file"""
         if os.path.exists(self.file_path):
             os.remove(self.file_path)
+            self.logger.info(f"Sensory file {self.file_path} deleted")
         else:
             self.logger.warning(f"Delete file '{self.file_path}' doesn't exist!")
