4064664014c156daa3ffe5fe177a91b6331c5138
Omar2535 <omar2535@alumni.ubc.ca>
2022-03-20 22:32:12 -0700

    finish basic implementation of sensory detector

--- sensory-detector/main.py
+++ sensory-detector/main.py
@@ -1,18 +1,47 @@
 from modules.CustomLogger import CustomLogger
 from modules.SensoryFile import SensoryFile
+from constants import CONFIG_FILE_PATH
+from utils.file_utils import get_list_of_sensor_paths
+
+from typing import List
+import yaml
+
+
+"""Global variable to hold our sensor file list"""
+sensor_files: List[SensoryFile] = []
 
 
 def main() -> None:
+    """Start up logger for main"""
     logger = CustomLogger(__name__).get_logger()
     logger.info("Starting up Rescate-tance sensory detector")
 
-    sf = SensoryFile("./lol.txt")
-    sf.create()
-    breakpoint()
-    sf.check()
-    sf.delete()
+    """Read and create file paths"""
+    config_file_stream = open(CONFIG_FILE_PATH, "r")
+    config = yaml.safe_load(config_file_stream)
+    dir_paths = config["directories_to_check"]
+    file_paths = get_list_of_sensor_paths(dir_paths)
+
+    """Create sensor files"""
+    for file_path in file_paths:
+        sensor_file = SensoryFile(file_path)
+        sensor_file.create()
+        sensor_files.append(sensor_file)
+
+    # TODO: Make infinite loop to check sensor files,
+    # then extend to kill ransomware when change is detected
+    """Check sensor files"""
+    for sensor_file in sensor_files:
+        sensor_file.check()
+
+    # TODO: Make a handler for sigint such that on sigint, the sensor files
+    # are deleted automatically
+    """Delete sensor files"""
+    for sensor_file in sensor_files:
+        sensor_file.delete()
 
     logger.info("Shutting down Rescate-tance sensory detector")
 
 
-main()
+if __name__ == "__main__":
+    main()
--- sensory-detector/utils/file_utils.py
+++ sensory-detector/utils/file_utils.py
@@ -1,3 +1,7 @@
+from typing import List
+from constants import DEFAULT_SENSOR_FILE_NAME
+
+
 # File utils
 def get_file_name_from_file_path(file_path: str) -> str:
     """Gets the file name from a file path
@@ -9,3 +13,18 @@ def get_file_name_from_file_path(file_path: str) -> str:
         str: File name
     """
     return file_path.split("/")[-1]
+
+
+def get_list_of_sensor_paths(dir_paths: List[str]) -> List[str]:
+    """Generate a list of file paths as directory + file_name
+
+    Args:
+        directories (List[str]): List of directories to have files in
+
+    Returns:
+        List[str]: List of file paths
+    """
+    file_path_list = []
+    for directory in dir_paths:
+        file_path_list.append(f"{directory.rstrip('//')}/{DEFAULT_SENSOR_FILE_NAME}")
+    return file_path_list
