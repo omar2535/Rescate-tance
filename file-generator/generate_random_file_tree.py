83e2909cd6bf82de93c316d19c3dcfc44dd637a4
Hao Zhu <hza164@sfu.ca>
2022-03-21 23:15:19 -0700

    implement random file and dir generator

---
+++ file-generator/generate_random_file_tree.py
@@ -0,0 +1,30 @@
+import itertools
+import pathlib
+import randomfiletree
+
+def callback(target_dir: pathlib.Path) -> pathlib.Path:
+    sourcedir = pathlib.Path("./templates")
+    sources = []
+    for srcfile in sourcedir.iterdir():
+        with open(srcfile, 'rb') as f:
+            content = f.read()
+        sources.append((srcfile.suffix, content))
+    
+    for srcfile in itertools.cycle(sources):
+        random_string = randomfiletree.core.random_string()
+        path = target_dir / (random_string + srcfile[0])
+        with path.open('wb') as f:
+            f.write(srcfile[1] + random_string.encode())
+        yield path
+
+# nfiles: file creation probability
+# nfolders: directory creation probability
+# payload: customize file name and content
+randomfiletree.core.iterative_gaussian_tree(
+    "./test_folder",
+    nfiles=10,
+    nfolders=10,
+    maxdepth=5,
+    repeat=4,
+    payload=callback
+)
