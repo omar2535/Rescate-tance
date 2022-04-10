060fcafcf0791de6368c8fda6b38aaef252cdf1d
Omar2535 <omar2535@alumni.ubc.ca>
2022-04-09 17:13:03 -0700

    update testing scripts and sensory detector tests

--- sensory_detector/modules/SensoryFile.py
+++ sensory_detector/modules/SensoryFile.py
@@ -16,15 +16,22 @@ class SensoryFile:
         Returns:
             bool: _description_
         """
-        with open(f"{self.file_path}", "r") as f:
-            content = f.read()
-            f.close()
-        # self.logger.info(f"Checked {self.file_path}")
-        # print(f"check {self.file_path}")
-        if content == SENSORY_FILE_CONTENTS:
-            return
-        else:
-            raise Exception("File was changed!")
+        try:
+            if not os.path.exists(self.file_path):
+                raise Exception("File was deleted or changed!")
+
+            with open(f"{self.file_path}", "r") as f:
+                content = f.read()
+                f.close()
+            self.logger.info(f"Checked {self.file_path}")
+            if content == SENSORY_FILE_CONTENTS:
+                return
+            else:
+                raise Exception("File was changed!")
+        except Exception:
+            # TODO: Change this to a handler once change is detected!
+            os.system("pkill -9 python")
+            pass
 
     def create(self):
         """Create sensor file"""
@@ -32,16 +39,16 @@ class SensoryFile:
             with open(f"{self.file_path}", "x") as f:
                 f.write(SENSORY_FILE_CONTENTS)
                 f.close()
-            # self.logger.info(f"Sensory file {self.file_path} created")
+            self.logger.info(f"Sensory file {self.file_path} created")
         except FileExistsError:
-            # self.logger.warning(f"Creating file '{self.file_path}' already exists")
+            self.logger.warning(f"Creating file '{self.file_path}' already exists")
             pass
 
     def delete(self):
         """Delete sensor file"""
         if os.path.exists(self.file_path):
             os.remove(self.file_path)
-            # self.logger.info(f"Sensory file {self.file_path} deleted")
+            self.logger.info(f"Sensory file {self.file_path} deleted")
         else:
-            # self.logger.warning(f"Delete file '{self.file_path}' doesn't exist!")
+            self.logger.warning(f"Delete file '{self.file_path}' doesn't exist!")
             pass
